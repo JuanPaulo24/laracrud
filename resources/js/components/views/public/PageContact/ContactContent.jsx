e7308480afae19e265cb854f2dd41158ded67053
j0th4r <jowehlazadar@gmail..com>
2025-02-03 14:49:56 +0800

    Change function name

--- resources/js/components/views/public/PageContact/ContactContent.jsx
+++ resources/js/components/views/public/PageContact/ContactContent.jsx
@@ -1,18 +1,4 @@
-/*
-  This example requires some changes to your config:
-
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-export default function Example() {
+export default function ContactContent() {
     return (
         <div className="relative isolate px-6 py-8 mb-12">
             <div className="mx-auto max-w-xl lg:max-w-4xl">
